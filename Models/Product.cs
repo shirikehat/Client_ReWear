c86fe17d060c11692b78a7ec91735cd3c81e4211
User <User@LAB3-3>
2024-12-25 15:40:02 +0200

    ddddd

---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_ReWear.Models
+{
+    internal class Product
+    {
+        public int ProductCode { get; set; }
+
+        public int Price { get; set; } 
+
+        public int UserId { get; set; } 
+
+        public string Size { get; set; }
+
+        public int StatusId { get; set; } 
+
+        public int TypeId { get; set; } 
+
+        public Product() { }
+    }
+}
