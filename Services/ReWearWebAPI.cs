607b1a80d511df6fdf7ba7d0793c11bbb2246cca
User <User@LAB3-3>
2025-03-25 15:23:43 +0200

    added

--- Services/ReWearWebAPI.cs
+++ Services/ReWearWebAPI.cs
@@ -695,4 +695,44 @@ public class ReWearWebAPI
             return;
         }
     }
+
+
+
+    public async Task<int?> PostProduct(Product product)
+    {
+        //Set URI to the specific function API
+        string url = $"{this.baseUrl}PostProduct";
+        try
+        {
+            //Call the server API
+            string json = JsonSerializer.Serialize(product);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(url, content);
+            //Check status
+            if (response.IsSuccessStatusCode)
+            {
+                //Extract the content as string
+                string resContent = await response.Content.ReadAsStringAsync();
+                //Desrialize result
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                int? result = JsonSerializer.Deserialize<int>(resContent, options);
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+
+
+
+
 }
--- ViewModels/PostViewModel.cs
+++ ViewModels/PostViewModel.cs
@@ -1,10 +1,365 @@
+using Client_ReWear.Models;
 using Client_ReWear.Services;
+using System.Collections.ObjectModel;
 namespace Client_ReWear.ViewModels;
 
 public class PostViewModel : ViewModelBase
 {
-	public PostViewModel()
+    public ReWearWebAPI proxy;
+
+
+    public ObservableCollection<PrType> PrTypes { get; set; }
+    public PostViewModel(ReWearWebAPI proxy)
 	{
-		
-	}
+        this.proxy = proxy;
+        this.PrTypes = new ObservableCollection<PrType>(((App)Application.Current).BasicData.PrTypes);
+        SelectedType = this.PrTypes[0];
+        CreateCommand = new Command(Create);
+
+        UploadPhotoCommand = new Command(OnUploadPhoto);
+        PhotoURL = proxy.GetDefaultProductPhotoUrl();
+        LocalPhotoPath = "";
+    }
+
+    #region Description
+
+    private bool showDescError;
+    public bool ShowDescError
+    {
+        get => showDescError;
+        set
+        {
+            showDescError = value;
+            OnPropertyChanged("ShowDescError");
+        }
+    }
+    private string description;
+
+    public string Description
+    {
+        get => description;
+        set
+        {
+            description = value;
+            ValidateDescription();
+            OnPropertyChanged("Description");
+        }
+    }
+    private string descriptionError;
+
+    public string DescriptionError
+    {
+        get => descriptionError;
+        set
+        {
+            descriptionError = value;
+            OnPropertyChanged("DescriptionError");
+        }
+    }
+    private void ValidateDescription()
+    {
+        if (string.IsNullOrEmpty(description) ||
+            description.Length < 5)
+        {
+            this.ShowDescError = true;
+        }
+        else
+            this.ShowDescError = false;
+    }
+    #endregion
+
+
+    #region Price
+
+    private bool showPriceError;
+    public bool ShowPriceError
+    {
+        get => showPriceError;
+        set
+        {
+            showPriceError = value;
+            OnPropertyChanged("ShowPriceError");
+        }
+    }
+    private int price;
+
+    public int Price
+    {
+        get => price;
+        set
+        {
+            price = value;
+            ValidatePrice();
+            OnPropertyChanged("Price");
+        }
+    }
+    private string priceError;
+
+    public string PriceError
+    {
+        get => priceError;
+        set
+        {
+            priceError = value;
+            OnPropertyChanged("PriceError");
+        }
+    }
+    private void ValidatePrice()
+    {
+        if (price == null)
+        {
+            this.ShowPriceError = true;
+        }
+        else
+            this.ShowPriceError = false;
+    }
+    #endregion
+
+    #region Store
+
+    private bool showStoreError;
+    public bool ShowStoreError
+    {
+        get => showStoreError;
+        set
+        {
+            showStoreError = value;
+            OnPropertyChanged("ShowStoreError");
+        }
+    }
+    private string store;
+
+    public string Store
+    {
+        get => store;
+        set
+        {
+            store = value;
+            ValidateStore();
+            OnPropertyChanged("Store");
+        }
+    }
+    private string storeError;
+
+    public string StoreError
+    {
+        get => storeError;
+        set
+        {
+            storeError = value;
+            OnPropertyChanged("StoreError");
+        }
+    }
+    private void ValidateStore()
+    {
+        if (string.IsNullOrEmpty(store))
+        {
+            this.ShowStoreError = true;
+        }
+        else
+            this.ShowStoreError = false;
+    }
+    #endregion
+
+    #region Size
+
+    private bool showSizeError;
+    public bool ShowSizeError
+    {
+        get => showSizeError;
+        set
+        {
+            showSizeError = value;
+            OnPropertyChanged("ShowSizeError");
+        }
+    }
+    private string size;
+
+    public string Size
+    {
+        get => size;
+        set
+        {
+            size = value;
+            ValidateSize();
+            OnPropertyChanged("Size");
+        }
+    }
+    private string sizeError;
+
+    public string SizeError
+    {
+        get => sizeError;
+        set
+        {
+            sizeError = value;
+            OnPropertyChanged("SizeError");
+        }
+    }
+    private void ValidateSize()
+    {
+        if (string.IsNullOrEmpty(size))
+        {
+            this.ShowStoreError = true;
+        }
+        else
+            this.ShowStoreError = false;
+    }
+    #endregion
+
+
+    #region Photo
+
+    private string photoURL;
+
+    public string PhotoURL
+    {
+        get => photoURL;
+        set
+        {
+            photoURL = value;
+            OnPropertyChanged("PhotoURL");
+        }
+    }
+
+    private string localPhotoPath;
+
+    public string LocalPhotoPath
+    {
+        get => localPhotoPath;
+        set
+        {
+            localPhotoPath = value;
+            OnPropertyChanged("LocalPhotoPath");
+        }
+    }
+
+    public Command UploadPhotoCommand { get; }
+    //This method open the file picker to select a photo
+    private async void OnUploadPhoto()
+    {
+        try
+        {
+            var result = await MediaPicker.Default.CapturePhotoAsync(new MediaPickerOptions
+            {
+                Title = "Please select a photo",
+            });
+
+            if (result != null)
+            {
+                // The user picked a file
+                this.LocalPhotoPath = result.FullPath;
+                this.PhotoURL = result.FullPath;
+            }
+        }
+        catch (Exception ex)
+        {
+        }
+
+    }
+
+    private void UpdatePhotoURL(string virtualPath)
+    {
+        Random r = new Random();
+        PhotoURL = proxy.GetImagesBaseAddress() + virtualPath + "?v=" + r.Next();
+        LocalPhotoPath = "";
+    }
+
+    #endregion
+
+
+    #region create product
+    public Command CreateCommand { get; }
+
+    public async void Create()
+    {
+        ValidateDescription();
+        ValidatePrice();
+        ValidateStore();
+        ValidateSize();
+
+        if (!ShowDescError && !ShowPriceError && !ShowStoreError && !ShowSizeError)
+        {
+
+            var newProduct = new Models.Product()
+            {
+                TypeId = SelectedType.TypeCode,
+                UserId = ((App)Application.Current).LoggedInUser.Id,
+                UserName = ((App)Application.Current).LoggedInUser.UserName,
+                Description = Description,
+                Price = Price,
+                Store = Store,
+                Size = Size
+
+            };
+
+            //Call the Register method on the proxy to register the new user
+            InServerCall = true;
+            int? productId = await proxy.PostProduct(newProduct);
+            InServerCall = false;
+
+            //If the registration was successful, navigate to the login page
+            if (productId != null)
+            {
+
+                //UPload profile imae if needed
+                if (!string.IsNullOrEmpty(LocalPhotoPath))
+                {
+                    
+                    Product? updatedProduct = await proxy.UploadProductImage(LocalPhotoPath);
+                    if (updatedProduct == null)
+                    {
+                        InServerCall = false;
+                        await Application.Current.MainPage.DisplayAlert("Posting product", "Product Data Was Saved BUT image upload failed", "ok");
+                    }
+                }
+
+
+
+                InServerCall = false;
+                newProduct.ProductCode = productId.Value;
+                ((App)(Application.Current)).MainPage.Navigation.PopAsync();
+            }
+            else
+            {
+
+                //If the registration failed, display an error message
+                string errorMsg = "Posting Product failed. Please try again.";
+                await Application.Current.MainPage.DisplayAlert("Posting Product", errorMsg, "ok");
+            }
+
+
+        }
+    }
+
+
+    #endregion
+
+        #region Type
+    private bool showTypeError;
+
+    public bool ShowTypeError
+    {
+        get => showTypeError;
+        set
+        {
+            showTypeError = value;
+            OnPropertyChanged("ShowNameError");
+        }
+    }
+
+    private PrType selectedType;
+
+    public PrType SelectedType
+    {
+        get => selectedType;
+        set
+        {
+            selectedType = value;
+            OnPropertyChanged("SelectedType");
+        }
+    }
+
+    #endregion
+
 }
